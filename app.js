3a3f39328cb4fd58125fb68407bb3986f19f3ae1
Philip-ZENG <892542757@qq.com>
2022-03-13 13:04:06 +0800

    update

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ var mysql = require('mysql');
 var bodyParser  = require("body-parser");
 var app = express();
 
+
 // Configure express application, set app configurations in the express framework
 // After setting this, the res.render() will look for files under /views folder and ends with .esj
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
+
 // Connecting to the mySQL server
 var connection = mysql.createConnection(
   {
-      host: 'localhost',
-      user: 'root',
+      host: 'database-2.c0pbv8ca91j5.us-east-1.rds.amazonaws.com',
+      user: 'admin',
       password : 'qweasdzxc',
-      database: 'join_us'
+      database: 'user'
   }
 );
 
@@ -31,7 +33,7 @@ connection.connect(function(err) {
 // Client gets something from the server
 app.get("/", function(req, res){
   // Find count of user in DB
-  var q = 'SELECT COUNT(*) AS count FROM users';
+  var q = 'SELECT COUNT(*) AS count FROM account';
   connection.query(q, function(err, result) {
     if(err) throw err;
     var count = result[0].count;
@@ -47,11 +49,11 @@ app.get("/", function(req, res){
 app.post('/register', function(req,res){
   // Obtain the client side info with the help of body-parser package
   var person = {
-    email: req.body.email
+    password: req.body.password
   };
 
   // Insert the data into database, the syntax comes from the mysql.js package
-  var q = "INSERT INTO users SET ?"
+  var q = "INSERT INTO account SET ?"
   connection.query(q, person, function(err, result){
     if(err) throw err;
     res.redirect("/"); //Redirect back to the given page
